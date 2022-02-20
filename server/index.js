ea8a7676c9b39c00bdfad64c8ca3f003d17b9958
Leon <leonsout@outlook.com>
2022-02-20 12:37:39 +0800

    添加koa-static

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@ const Koa = require('koa');
 const { port } = require('./config');
 const koaBody = require('koa-body');
 const cors = require('@koa/cors');
+const static = require('koa-static');
 const router = require('./router');
 
 const app = new Koa();
 require('./utils/db')();
 app.use(cors());
 app.use(koaBody());
+app.use(static(__dirname + '/public/'));
 
 app.use(router.routes()).use(router.allowedMethods());
 
